614e7df1e1ea120a1676bf1f1f2e56fb992f0a24
shalom333-cpu <elishaolimbas@gmail.com>
2021-02-28 17:42:37 +0300

    add edit form section

--- js/scripts.js
+++ js/scripts.js
@@ -23,4 +23,17 @@ $(document).ready(function () {
     $("#product").slideUp('1500');
     $("#product-image").slideDown('1500');
   });
+
+  $("form#form24").submit(function (event) {
+    var name = $("input#MERGE1").val();
+    var email = $("input#MERGE0").val();
+    var message = $("textarea#comment").val();
+    if ($("input#MERGE1").val() && $("input#MERGE0").val()) {
+      alert(name + ", we have received your message. Thank you for reaching out to us.");
+    }
+    else {
+      alert("Please enter your name and email!");
+    }
+
+  });
 });
