0bf73a4009881157a7f7fce8309448ff742cc501
shalom333-cpu <elishaolimbas@gmail.com>
2021-02-28 16:26:05 +0300

    add effects to design section

--- js/scripts.js
+++ js/scripts.js
@@ -7,4 +7,12 @@ $(document).ready(function () {
     $("#development").slideUp('1500');
     $("#development-image").slideDown('1500');
   });
+  $("#design-image").click(function () {
+    $("#design-image").slideDown('1500').hide('1000');
+    $("#design").show('1500');
+  });
+  $("#design").click(function () {
+    $("#design").slideUp('1500');
+    $("#design-image").slideDown('1500');
+  });
 });
